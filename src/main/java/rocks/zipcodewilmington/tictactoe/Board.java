bc995f4df8f2275bf9448fe6b64bcc327f09f2e9
Joseph Kowalski <jkjr25@gmail.com>
2022-03-04 13:10:05 -0500

    Added in favor of x

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,11 +4,30 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    public  Character [][] tictactoeBoard;
+
     public Board(Character[][] matrix) {
+        tictactoeBoard = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+
+        if (tictactoeBoard[0][0] == 'X' && tictactoeBoard[0][1] == 'X' && tictactoeBoard[0][2] == 'X')
+            return true;
+        if (tictactoeBoard[1][0] == 'X' && tictactoeBoard[1][1] == 'X' && tictactoeBoard[1][2] == 'X')
+            return true;
+        if (tictactoeBoard[2][0] == 'X' && tictactoeBoard[2][1] == 'X' && tictactoeBoard[2][2] == 'X')
+            return true;
+        if (tictactoeBoard[0][0] == 'X' && tictactoeBoard[1][0] == 'X' && tictactoeBoard[2][0] == 'X')
+            return true;
+        if (tictactoeBoard[0][1] == 'X' && tictactoeBoard[1][1] == 'X' && tictactoeBoard[1][2] == 'X')
+            return true;
+        if (tictactoeBoard[0][2] == 'X' && tictactoeBoard[2][2] == 'X' && tictactoeBoard[2][2] == 'X')
+            return true;
+        if (tictactoeBoard[0][0] == 'X' && tictactoeBoard[1][1] == 'X' && tictactoeBoard[2][2] == 'X')
+            return true;
+        if (tictactoeBoard[0][2] == 'X' && tictactoeBoard[1][1] == 'X' && tictactoeBoard[2][0] == 'X')
+            return true;
     }
 
     public Boolean isInFavorOfO() {

9682cce63cab7f7a810f915d53d1ff8c334ce1d2
Joseph Kowalski <jkjr25@gmail.com>
2022-03-04 13:22:12 -0500

    Added rest of in favor of O, tie, and get winner

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -11,19 +11,21 @@ public class Board {
     }
 
     public Boolean isInFavorOfX() {
-
+        //horizontal win
         if (tictactoeBoard[0][0] == 'X' && tictactoeBoard[0][1] == 'X' && tictactoeBoard[0][2] == 'X')
             return true;
         if (tictactoeBoard[1][0] == 'X' && tictactoeBoard[1][1] == 'X' && tictactoeBoard[1][2] == 'X')
             return true;
         if (tictactoeBoard[2][0] == 'X' && tictactoeBoard[2][1] == 'X' && tictactoeBoard[2][2] == 'X')
             return true;
+        //vertical win
         if (tictactoeBoard[0][0] == 'X' && tictactoeBoard[1][0] == 'X' && tictactoeBoard[2][0] == 'X')
             return true;
         if (tictactoeBoard[0][1] == 'X' && tictactoeBoard[1][1] == 'X' && tictactoeBoard[1][2] == 'X')
             return true;
-        if (tictactoeBoard[0][2] == 'X' && tictactoeBoard[2][2] == 'X' && tictactoeBoard[2][2] == 'X')
+        if (tictactoeBoard[0][2] == 'X' && tictactoeBoard[1][2] == 'X' && tictactoeBoard[2][2] == 'X')
             return true;
+        //diagonal win
         if (tictactoeBoard[0][0] == 'X' && tictactoeBoard[1][1] == 'X' && tictactoeBoard[2][2] == 'X')
             return true;
         if (tictactoeBoard[0][2] == 'X' && tictactoeBoard[1][1] == 'X' && tictactoeBoard[2][0] == 'X')
@@ -31,15 +33,39 @@ public class Board {
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        //horizontal win
+        if (tictactoeBoard[0][0] == 'O' && tictactoeBoard[0][1] == 'O' && tictactoeBoard[0][2] == 'O')
+            return true;
+        if (tictactoeBoard[1][0] == 'O' && tictactoeBoard[1][1] == 'O' && tictactoeBoard[1][2] == 'O')
+            return true;
+        if (tictactoeBoard[2][0] == 'O' && tictactoeBoard[2][1] == 'O' && tictactoeBoard[2][2] == 'O')
+            return true;
+        //vertical win
+        if (tictactoeBoard[0][0] == 'O' && tictactoeBoard[1][0] == 'O' && tictactoeBoard[2][0] == 'O')
+            return true;
+        if (tictactoeBoard[0][1] == 'O' && tictactoeBoard[1][1] == 'O' && tictactoeBoard[1][2] == 'O')
+            return true;
+        if (tictactoeBoard[0][2] == 'O' && tictactoeBoard[1][2] == 'O' && tictactoeBoard[2][2] == 'O')
+            return true;
+        //diagonal win
+        if (tictactoeBoard[0][0] == 'O' && tictactoeBoard[1][1] == 'O' && tictactoeBoard[2][2] == 'O')
+            return true;
+        if (tictactoeBoard[0][2] == 'O' && tictactoeBoard[1][1] == 'O' && tictactoeBoard[2][0] == 'O')
+            return true;
     }
 
     public Boolean isTie() {
-        return null;
+        if (isInFavorOfX() || isInFavorOfO())
+            return false;
+        else
+            return true;
     }
 
     public String getWinner() {
-        return null;
-    }
+        if (isInFavorOfX())
+            return "X";
+        else if (isInFavorOfO())
+            return "O";
 
+    }
 }
